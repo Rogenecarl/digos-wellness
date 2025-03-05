e7c1bbe53aaffedbbb1ad5c09a3671e6c4af2300
Rogenecarl <chessgame221@gmail.com>
2025-03-05 19:17:10 +0800

    Fix Vercel output directory

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // Explicitly set the output directory to 'dist'
+  },
 });
